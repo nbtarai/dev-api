adb2622a4e3fc7a4b0260dc81b4e8caf26c7254c
nbtarai <t.arai@newbees.jp>
2018-11-21 15:22:45 +0900

    Api追加

---
+++ api/mail_multi_send.php
@@ -0,0 +1,19 @@
+<?php
+
+$userIds = explode(',', $_REQUEST['user_id']);
+
+$userIdsLen = count($userIds);
+
+$okUserList = [];
+$ngUserList = [];
+for ($i = 0; $i < $userIdsLen; $i++) {
+    $okUserList[] = ['user_id' => $userIds[$i]];
+}
+
+$data = ['ok_user_list' => $okUserList, 'ng_user_list' => $ngUserList];
+
+$retData = ['code' => 200, 'message' => 'test', 'data' => $data, 'user' => []];
+
+echo json_encode($retData);
+
+?>
