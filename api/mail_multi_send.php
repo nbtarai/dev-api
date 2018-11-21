d7e0e776e5be0111acccb905a2f2d05373ca3f76
nbtarai <t.arai@newbees.jp>
2018-11-21 15:36:09 +0900

    修正

--- api/mail_multi_send.php
+++ api/mail_multi_send.php
@@ -1,5 +1,7 @@
 <?php
 
+$OK_COUNT = 5;
+
 $userIds = explode(',', $_REQUEST['user_id']);
 
 $userIdsLen = count($userIds);
@@ -7,7 +9,11 @@ $userIdsLen = count($userIds);
 $okUserList = [];
 $ngUserList = [];
 for ($i = 0; $i < $userIdsLen; $i++) {
-    $okUserList[] = ['user_id' => $userIds[$i]];
+    if ($i < $OK_COUNT) {
+        $okUserList[] = ['user_id' => $userIds[$i]];
+    } else {
+        $ngUserList[] = ['user_id' => $userIds[$i]];
+    }
 }
 
 $data = ['ok_user_list' => $okUserList, 'ng_user_list' => $ngUserList];
